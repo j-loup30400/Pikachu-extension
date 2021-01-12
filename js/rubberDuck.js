44ef2747602a169c698fa95e3e712693e9019f7b
mancia08 <teikomancini1993@gmail.com>
2021-01-12 22:09:42 +0100

    fix: background image taken as well

--- js/rubberDuck.js
+++ js/rubberDuck.js
@@ -1,4 +1,15 @@
-let marc="chrome-extension://pmmblmlopmppgnikehndlnkaminfjfkc/images/icon.png"
-let images = Array.from(document.getElementsByTagName("img"));
-
-images.map(image => image.setAttribute("src", marc))
+let marc =
+  "chrome-extension://pmmblmlopmppgnikehndlnkaminfjfkc/images/icon.png";
+let images = [];
+let bgImages = [];
+window.addEventListener("load", () => {
+  images = Array.from(document.getElementsByTagName("img"));
+  bgImages = Array.from(document.getElementsByTagName("div"));
+  images.map(image => image.setAttribute("src", marc));
+  console.log(bgImages);
+  bgImages.map(element =>
+    element.style.backgroundImage.includes("url")
+      ? (element.style.backgroundImage = `url(${marc})`)
+      : console.log("no")
+  );
+});
